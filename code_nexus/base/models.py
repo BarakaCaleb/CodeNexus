ebaf5c029b9fb813e57ad054c20f49a917d77bf8
Breezekid <calebbaraka79@gmail.com>
2024-04-03 20:42:00 +0300

    Created more models

--- code_nexus/base/models.py
+++ code_nexus/base/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
     #host =
@@ -14,3 +21,15 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.body[:50]
